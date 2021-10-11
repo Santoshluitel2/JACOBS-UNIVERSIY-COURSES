0d52d700ceabb89eb40644d3908dad6a33e611d3
santosh luitel <90577431+Santoshluitel2@users.noreply.github.com>
2021-10-11 22:27:10 +0200

    Add files via upload

---
+++ computer vision/imageManip.py
@@ -0,0 +1,221 @@
+import math
+
+import numpy as np
+from PIL import Image
+from skimage import color, io
+
+
+def load(image_path):
+    """Loads an image from a file path.
+
+    HINT: Look up `skimage.io.imread()` function.
+
+    Args:
+        image_path: file path to the image.
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+    out = None
+
+    ### YOUR CODE HERE
+    # Use skimage io.imread
+    out=io.imread(image_path) # reading image
+    pass
+    ### END YOUR CODE
+
+    # Let's convert the image to be between the correct range.
+    out = out.astype(np.float64) / 255
+    return out
+
+
+def dim_image(image):
+    """Change the value of every pixel by following
+
+                        x_n = 0.5*x_p^2
+
+    where x_n is the new value and x_p is the original value.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+
+    out = None
+
+    ### YOUR CODE HERE
+    pass
+    ### END YOUR CODE
+
+    return 0.5*image**2 # changing the value of pixel
+
+
+def convert_to_grey_scale(image):
+    """Change image to gray scale.
+
+    HINT: Look at `skimage.color` library to see if there is a function
+    there you can use.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+
+    Returns:
+        out: numpy array of shape(image_height, image_width).
+    """
+    out = None
+
+    ### YOUR CODE HERE
+    
+    pass
+    ### END YOUR CODE
+
+    return color.rgb2gray(image) # converting to grayscale
+
+
+def rgb_exclusion(image, channel):
+    """Return image **excluding** the rgb channel specified
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+        channel: str specifying the channel. Can be either "R", "G" or "B".
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+
+    out = None
+
+    ### YOUR CODE HERE
+    out = image.copy() #using if condition to check character and excluding the color
+    if channel== 'R':
+         out[:,:,0] = 0
+    elif channel== 'G':
+        out[:,:,1]=0
+    else :
+        out[:,:,2]=0
+    
+    pass
+    ### END YOUR CODE
+
+    return out
+
+
+def lab_decomposition(image, channel):
+    """Decomposes the image into LAB and only returns the channel specified.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+        channel: str specifying the channel. Can be either "L", "A" or "B".
+
+    Returns:
+        out: numpy array of shape(image_height, image_width).
+    """
+
+    
+    out = None
+    out = color.rgb2lab(image)
+
+    ### YOUR CODE HERE
+    pass
+    ### END YOUR CODE
+
+    return out
+
+
+def hsv_decomposition(image, channel='H'):
+    """Decomposes the image into HSV and only returns the channel specified.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+        channel: str specifying the channel. Can be either "H", "S" or "V".
+
+    Returns:
+        out: numpy array of shape(image_height, image_width).
+    """
+
+    
+    out = None
+
+    ### YOUR CODE HERE
+    out = color.rgb2hsv(image)
+    if channel== 'H' :  #using if condition to check character and excluding the colors
+            out[:,:,1] = 0
+            out[:,:,2]=0
+    elif channel== 'S' :
+               out[:,:,0]=0
+               out[:,:,2]=0
+                
+    else :
+        out[:,:,1]=0
+        out[:,:,0]=0
+    
+    pass
+    ### END YOUR CODE
+
+    return out
+
+
+def mix_images(image1, image2, channel1, channel2):
+    """Combines image1 and image2 by taking the left half of image1
+    and the right half of image2. The final combination also excludes
+    channel1 from image1 and channel2 from image2 for each image.
+
+    HINTS: Use `rgb_exclusion()` you implemented earlier as a helper
+    function. Also look up `np.concatenate()` to help you combine images.
+
+    Args:
+        image1: numpy array of shape(image_height, image_width, 3).
+        image2: numpy array of shape(image_height, image_width, 3).
+        channel1: str specifying channel used for image1.
+        channel2: str specifying channel used for image2.
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+
+    out = None
+    
+    ### YOUR CODE HERE
+    temp1=image1.copy()#copying image so orginal is not editied
+    temp2=image2.copy()
+    temp1 = temp1[:, :int(temp1.shape[0] / 2)] #for left side of first image
+    temp2 = temp2[:, int(temp2.shape[0] / 2):] # for right side of second image
+    temp1= rgb_exclusion(temp1,channel1) #for func for excluding color
+    temp2= rgb_exclusion(temp2,channel2)
+    out= np.concatenate((temp1, temp2), axis=1) #adding 
+    
+    pass
+    ### END YOUR CODE
+
+    return out
+
+
+def mix_quadrants(image):
+    """THIS IS AN EXTRA CREDIT FUNCTION.
+
+    This function takes an image, and performs a different operation
+    to each of the 4 quadrants of the image. Then it combines the 4
+    quadrants back together.
+
+    Here are the 4 operations you should perform on the 4 quadrants:
+        Top left quadrant: Remove the 'R' channel using `rgb_exclusion()`.
+        Top right quadrant: Dim the quadrant using `dim_image()`.
+        Bottom left quadrant: Brighthen the quadrant using the function:
+            x_n = x_p^0.5
+        Bottom right quadrant: Remove the 'R' channel using `rgb_exclusion()`.
+
+    Args:
+        image1: numpy array of shape(image_height, image_width, 3).
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+    out = None
+
+    ### YOUR CODE HERE
+    pass
+    ### END YOUR CODE
+
+    return out
